a71106eb1ef5cce92f6742f7f71d633d55a4d9a1
Capotss <felipebittarmartiniano@gmail.com>
2023-10-30 18:47:08 -0300

    lab3 final

--- teste.c
+++ teste.c
@@ -1,8 +1,13 @@
-// dev
+int main()
+{
 
-int main(){
+    char b[10];
+    char c[10];
+    // printf("size of b: %s", sizeof(b));
+    printf("size of b: %llu", sizeof(b));
+    scanf("%s",&b);
+    printf("b: %s", b);
+    printf("size of b: %llu", sizeof(b));
 
 
-    
-    return 0;
 }
